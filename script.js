025242938b2095ed5176bea692a4730d80d92e98
yuiohnuki <ohnukiyui1111@gmail.com>
2025-10-17 01:05:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,64 +1,75 @@
 var introvertScore = 0;
 var extrovertScore = 0;
-
 var questionCount = 0;
 
 var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
-
 var q2a1 = document.getElementById("q2a1");
 var q2a2 = document.getElementById("q2a2");
-
 var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
-
 var result = document.getElementById("result");
+var restartButton = document.getElementById("restart");
 
+// Event listeners for answer buttons
 q1a1.addEventListener("click", introvert);
 q1a2.addEventListener("click", extrovert);
-
 q2a1.addEventListener("click", introvert);
 q2a2.addEventListener("click", extrovert);
-
 q3a1.addEventListener("click", introvert);
 q3a2.addEventListener("click", extrovert);
+restartButton.addEventListener("click", restartQuiz);
 
+// Function when choosing introvert
 function introvert() {
-  catScore += 1;
+  introvertScore += 1;
   questionCount += 1;
-  
-  console.log("questionCount =" + questionCount + "introvertScore = " + introvertScore); 
-  if (questionCount == 3){
-  console.log("This quiz is done!");
-  updateResult();
+
+  console.log("questionCount = " + questionCount + " introvertScore = " + introvertScore); 
+  if (questionCount === 3) {
+    console.log("This quiz is done!");
+    updateResult();
   }   
 }
+
+// Function when choosing extrovert
 function extrovert() {
-  extrovertScore += 1;          
+  extrovertScore += 1;
   questionCount += 1;
+
   console.log("questionCount = " + questionCount + " extrovertScore = " + extrovertScore);
-  if (questionCount == 3){
-  console.log("This quiz is done!");
-updateResult();
-}   
+  if (questionCount === 3) {
+    console.log("This quiz is done!");
+    updateResult();
+  }   
 }                                                                
 
+// Function to update result based on scores
 function updateResult() {
-  if (introvertScore >= 2){
+  if (introvertScore >= 2) {
     result.innerHTML = "You are an introvert!";
     console.log("You are an introvert!");
-} else if (extrovertScore >= 2) {
+  } else if (extrovertScore >= 2) {
     result.innerHTML = "You are an extrovert!";
     console.log("You are an extrovert!");
+  }
+
+  // Show restart button
+  restartButton.style.display = "block";
 }
-}   
 
-var result = document.getElementById("result");
+// Function to restart quiz
+function restartQuiz() {
+  introvertScore = 0;
+  extrovertScore = 0;
+  questionCount = 0;
 
-var restartButton = document.getElementById("restart").innerHTML = "Restart the quiz!";
+  result.innerHTML = "Your result is...";  // Reset result display
+  console.log("Quiz restarted!");
 
-restartButton.addEventListener("click", updateRestart);
-
-function updateRestart(){
-  document.getElementById("restart").innerHTML = "Restart the quiz!";
+  // Hide restart button again
+  restartButton.style.display = "none";
 }
+
+// Hide restart button initially
+restartButton.style.display = "none";
